a12f340ae62e7f24da9d90a42d4f046d666b47cf
pizza <pizzahaut@gmail.com'>
2017-11-08 17:04:40 -0500

    fixed addvert

---
+++ src/Graph.java
@@ -0,0 +1,12 @@
+/**
+ *Graph.java -- interface for our graph classes 
+ */
+
+/**
+ * @author pizza
+ *
+ */
+public interface Graph {
+	Graph create(int numVerts);
+	
+}
---
+++ src/grapher.java
@@ -0,0 +1,36 @@
+/**
+ *make two child classes AdjMatrix AdjGraph
+ *and AdjListGraph
+ *make both of the children of this and fill in each method 
+ */
+
+/**
+ * @author pizza
+ *
+ */
+
+public abstract class grapher {
+	
+	abstract grapher create(int numVerts);
+	abstract grapher create();
+	
+	abstract void addVert();
+	
+	abstract boolean addEdge(int v1, int v2);
+	abstract boolean addEdge(int v1, int v2, double wt);
+	
+	abstract boolean neighbors(int v1, int v2);
+	
+	abstract double edgeWt(int v1, int v2);
+	
+	abstract boolean markVertex(int v1);
+	abstract boolean markedVertex(int v1);
+	abstract boolean clearVertexMarker(int v1);
+	
+	abstract boolean clearEdgeMarker(int v1, int v2);
+	abstract boolean markEdge(int v1, int v2);
+	abstract boolean markedEdge(int v1, int v2);
+	
+	
+	
+}
